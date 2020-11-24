5ad4040a8c3f3f86200ff59367b086a46ef42a15
Jhay Pace <pacejhay.ict@gmail.com>
2020-11-24 19:55:37 +0800

    first commit

---
+++ home/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from home import views
+
+urlpatterns = [
+    path('' , views.index, name ='home'),
+    path('update/<str:pk>/', views.updateTask, name = "update"),
+    path('delete/<str:pk>/', views.deleteTask, name = "delete"),
+    path('search' , views.search, name = 'search')
+
+
+    
+]
